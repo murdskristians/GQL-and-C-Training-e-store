99519ff4e7e7eb545fcdccdf6d855dfb73406e09
Kristians <murdskristians@gmail.com>
2024-11-23 19:08:17 +0200

    add post for stock

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0844e3cc565d5a34bafba2055e1ed9976afeb9a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c18a683ad423fa722f3d8989b0bf50988bada94")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
