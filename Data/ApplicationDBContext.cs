6c036e1a14bfab818fc35e41a756f7300354bd97
Kristians <murdskristians@gmail.com>
2025-01-05 14:54:20 +0200

    Add registration and roles

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -5,18 +5,32 @@ using System.Threading.Tasks;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace api.Data
 {
-    public class ApplicationDbContext: IdentityDbContext<AppUser>
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
         {
-            
+
         }
 
         public DbSet<Stock> Stocks { get; set; }
-        public DbSet<Comment> Comments { get; set; }    
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole { Id = "Admin", Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Id = "User", Name = "User", NormalizedName = "USER" }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
